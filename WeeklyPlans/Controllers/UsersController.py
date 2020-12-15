3d20e4e9e4ddc2ad88db5c0c8d1860c43ba04ceb
jenniferasmar <60263256+jenniferasmar@users.noreply.github.com>
2020-12-15 16:56:11 -0500

    Users model addded and first api call implemented

---
+++ WeeklyPlans/Controllers/UsersController.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+from Service.UsersService import UsersService
+
+app = Flask(__name__)
+
+
+@app.route("/createUser", methods=['POST'])
+def create():
+    data = request.data
+    return UsersService.create_user(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
