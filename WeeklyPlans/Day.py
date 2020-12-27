a29a26ff7992fabcbc4317eb4910db0fe856158a
sham-a <ayahshamma123@gmail.com>
2020-12-26 21:08:55 -0500

    created base classes for task and day

---
+++ WeeklyPlans/Day.py
@@ -0,0 +1,25 @@
+import enum
+from Task import Task
+
+
+class DayOfWeek(enum.Enum):
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+    Sun = 7
+
+
+class Day:
+
+    def __init__(self, day_type: DayOfWeek, tasks=None):
+        self.dayType = day_type
+        if tasks is None:
+            self.tasks = list[Task]
+        else:
+            self.tasks = tasks
+
+    def add_task(self, new_task: Task):
+        self.tasks.append(new_task)
