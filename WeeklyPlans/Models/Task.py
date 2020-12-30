679709c51b7ddc3bd460ba9a2a9026f1872ae226
sham-a <ayahshamma123@gmail.com>
2020-12-30 16:34:57 -0500

    added list of days to tasks

--- WeeklyPlans/Models/Task.py
+++ WeeklyPlans/Models/Task.py
@@ -1,4 +1,5 @@
 import enum
+from Models.Day import Day
 
 
 class TaskType(enum.Enum):
@@ -8,11 +9,12 @@ class TaskType(enum.Enum):
 
 
 class Task:
-    def __init__(self, task_name: str, task_type: TaskType, task_start: float, task_duration: float):
+    def __init__(self, task_name: str, task_type: TaskType, task_start: float, task_duration: float, task_days: [Day]):
         self.title = task_name
         self.type = task_type
         self.start_time = task_start
         self.duration = task_duration
+        self.days = task_days
         self.repeat = False
 
     def set_repeat(self, boolean: bool):
